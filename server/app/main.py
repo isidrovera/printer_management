dced6248fa9bd75925e7d9ef2e787d764b6644b4
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-23 15:21:58 -0500

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -4,15 +4,13 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.core.config import settings
 from app.api.v1.api import api_router
-from app.api.v1.endpoints import web
 from app.db.session import engine
-from app.db.base import Base  # Agrega esta línea
+from app.db.base import Base
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
-app.include_router(api_router, prefix="/api/v1")
-app.include_router(web.router)
+app.include_router(api_router)  # Quita el prefix
 
 Base.metadata.create_all(bind=engine)
