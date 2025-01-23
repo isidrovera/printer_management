125e617ed8058ef8e10437eaf3344ad0a528586e
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-23 15:10:36 -0500

    Update __init__.py

--- server/app/db/models/__init__.py
+++ server/app/db/models/__init__.py
@@ -2,6 +2,6 @@ from .agent import Agent
 from .client import Client
 from .printer_driver import PrinterDriver
 from .printer_job import PrinterJob
-from . import base
+
 
 __all__ = ['Agent', 'Client', 'PrinterDriver', 'PrinterJob']
