b4c72c1cc32c1c0f6a64808357ef529014a156c9
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-23 14:30:25 -0500

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -5,8 +5,16 @@ from app.api.v1.api import api_router
 from app.db.session import engine
 from app.db.base import Base
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    # Permitir todos los orígenes
+    root_path="/",
+    openapi_url="/openapi.json",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
 app.include_router(api_router, prefix="/api/v1")
 
-# Crear tablas en la base de datos
+# Crear tablas
 Base.metadata.create_all(bind=engine)
