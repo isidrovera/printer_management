f9a04d5ac5302c035e429878fb75690f802f25f0
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-23 15:18:53 -0500

    ..

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -1,4 +1,3 @@
-# app/api/v1/api.py
 from fastapi import APIRouter
 from app.api.v1.endpoints import agents, websocket, web
 
@@ -6,4 +5,3 @@ api_router = APIRouter()
 api_router.include_router(agents.router, prefix="/agents", tags=["agents"])
 api_router.include_router(websocket.router, prefix="/ws", tags=["websocket"])
 api_router.include_router(web.router, tags=["web"])
-
