424c61196ed3173b0b5dd223972d33c0e81b4809
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-24 16:57:39 -0500

    Update printer_driver.py

--- server/app/db/models/printer_driver.py
+++ server/app/db/models/printer_driver.py
@@ -7,7 +7,6 @@ class PrinterDriver(Base):
 
     id = Column(Integer, primary_key=True, index=True)  # ID único
     manufacturer = Column(String, nullable=False)  # Nombre del fabricante
-    model = Column(String, nullable=False)  # Modelo de la impresora
-    driver_file = Column(LargeBinary, nullable=False)  # Archivo binario (ZIP)
+    model = Column(String, nullable=False)  # Modelo de la impresora    
     driver_filename = Column(String, nullable=False)  # Nombre original del archivo
     description = Column(String, nullable=True)  # Descripción opcional
