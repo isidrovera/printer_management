a1639323042aecffde07830087cb63e79bcb003b
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-01-27 17:09:49 -0500

    Update drivers.py

--- server/app/api/v1/endpoints/drivers.py
+++ server/app/api/v1/endpoints/drivers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.db.database import get_db
+from app.db.session import get_db
 from app.services.driver_service import DriverService
 from typing import List
 
