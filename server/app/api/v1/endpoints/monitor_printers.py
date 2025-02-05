a91c6be605e28d06a4286e27c7574fb990c2ac42
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-04 21:24:04 -0500

    Save monitor_printers.py.

--- server/app/api/v1/endpoints/monitor_printers.py
+++ server/app/api/v1/endpoints/monitor_printers.py
@@ -1,5 +1,5 @@
 # server/app/api/v1/endpoints/monitor_printers.py
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Recuest
 from sqlalchemy.orm import Session
 from typing import Dict, Any, List
 
