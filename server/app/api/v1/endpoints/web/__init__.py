1ab14f3ed536ee949b35eef3bf9eb5f2be20d099
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-08 20:01:49 +0000

    .

--- server/app/api/v1/endpoints/web/__init__.py
+++ server/app/api/v1/endpoints/web/__init__.py
@@ -1,7 +1,16 @@
 #server\app\api\v1\endpoints\web\__init__.py
 from fastapi import APIRouter
+from . import dashboard, clients, agents, drivers, tunnels, monitor_printers, printer_oids
 
 router = APIRouter()
 
-# Exportar el router
+# Incluir todos los routers web
+router.include_router(dashboard.router)
+router.include_router(clients.router)
+router.include_router(agents.router)
+router.include_router(drivers.router)
+router.include_router(tunnels.router)
+router.include_router(monitor_printers.router)
+router.include_router(printer_oids.router)
+
 __all__ = ["router"]
