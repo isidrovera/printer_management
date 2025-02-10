f5da7a5b138a5b83d16204a9dab7740489758e73
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-10 10:48:15 -0500

    Update main.py

--- server/app/main.py
+++ server/app/main.py
@@ -20,6 +20,7 @@ from app.core.config import settings
 from app.api.v1.api import api_router, web_router
 from app.db.session import engine
 from app.db.base import Base
+from app.db.session import SessionLocal
 
 logger.info("Starting application...")
 
