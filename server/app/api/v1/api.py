a505e0904dfb58382198cf1f68b002cab97ffcf4
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-10 10:24:48 -0500

    Update api.py

--- server/app/api/v1/api.py
+++ server/app/api/v1/api.py
@@ -1,78 +1,88 @@
 # server/app/api/v1/api.py
 from fastapi import APIRouter
-from app.api.v1.endpoints import agents, websocket, web, printers, drivers, tunnels, monitor_printers, printer_oids, dashboard, auth
+from app.api.v1.endpoints import (
+   agents, websocket, web, printers, drivers, 
+   tunnels, monitor_printers, printer_oids, 
+   dashboard, auth
+)
 
 # Router para APIs
 api_router = APIRouter()
+
+# Router web separado 
+web_router = APIRouter()
+
+# Rutas de autenticación
 api_router.include_router(
-    dashboard.router,
-    prefix="/dashboard",
-    tags=["dashboard"]
+   auth.router,
+   prefix="/auth",
+   tags=["auth"]
+)
+
+# Rutas de dashboard
+api_router.include_router(
+   dashboard.router,
+   prefix="/dashboard", 
+   tags=["dashboard"]
 )
 
 # Rutas para drivers
 api_router.include_router(
-    drivers.router, 
-    prefix="/drivers", 
-    tags=["drivers"]
+   drivers.router,
+   prefix="/drivers",
+   tags=["drivers"]
 )
 
 # Rutas para agentes
 api_router.include_router(
-    agents.router, 
-    prefix="/agents", 
-    tags=["agents"]
+   agents.router,
+   prefix="/agents",
+   tags=["agents"]
 )
 
 # Rutas para websocket
 api_router.include_router(
-    websocket.router, 
-    prefix="/ws", 
-    tags=["websocket"]
+   websocket.router,
+   prefix="/ws",
+   tags=["websocket"]
 )
 
 # Rutas para impresoras
 api_router.include_router(
-    printers.router, 
-    prefix="/printers", 
-    tags=["printers"]
+   printers.router,
+   prefix="/printers",
+   tags=["printers"]
 )
 
 # Rutas para túneles
 api_router.include_router(
-    tunnels.router, 
-    prefix="/tunnels", 
-    tags=["tunnels"]
+   tunnels.router,
+   prefix="/tunnels",
+   tags=["tunnels"]
 )
 
 # Rutas para monitoreo de impresoras
 api_router.include_router(
-    monitor_printers.router, 
-    prefix="/monitor/printers", 
-    tags=["monitor_printers"]
+   monitor_printers.router,
+   prefix="/monitor/printers",
+   tags=["monitor_printers"]
 )
 
 # Rutas para OIDs de impresoras
 api_router.include_router(
-    printer_oids.router, 
-    prefix="/printer-oids", 
-    tags=["printer-oids"]
+   printer_oids.router,
+   prefix="/printer-oids",
+   tags=["printer-oids"]
 )
-# Añade las rutas de autenticación
-api_router.include_router(
-    auth.router,
-    prefix="/auth",
-    tags=["auth"]
+
+# Incluir router web y de autenticación
+web_router.include_router(
+   web.router,
+   tags=["web"]
 )
 
 web_router.include_router(
-    auth.router,
-    prefix="/auth",
-    tags=["auth"]
-)
-# Router web separado
-web_router = APIRouter()
-web_router.include_router(
-    web.router, 
-    tags=["web"]
+   auth.router,
+   prefix="/auth",
+   tags=["auth"]
 )
