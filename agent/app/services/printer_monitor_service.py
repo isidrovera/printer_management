6e490c95567a79d93ed3c578a525f9806e1915e6
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-15 02:17:14 +0000

    ...

--- agent/app/services/printer_monitor_service.py
+++ agent/app/services/printer_monitor_service.py
@@ -614,7 +614,7 @@ class PrinterMonitorService:
                 return False
             
             async with aiohttp.ClientSession() as session:
-                base_url = self.server_url.replace('ws://', 'http://')
+                base_url = self.server_url.replace('wss://', 'https://')
                 url = f"{base_url}/api/v1/monitor/printers/update"
                 
                 headers = {
