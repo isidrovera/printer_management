a66bb484fbbf36830064f512482b9c1fa19a164f
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-20 14:50:13 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permitir acceso desde IP externa
+    port: 3000, // Puerto específico
+    strictPort: true, // Usar estrictamente este puerto
+  },
 })
