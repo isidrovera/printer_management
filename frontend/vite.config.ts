7cd3a6a139d3c2835b75f651f096b4fb45857ef5
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-21 22:05:14 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     proxy: {
-      '^/(?!assets).*': {  // Todo excepto /assets
+      '/auth': {
         target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false
