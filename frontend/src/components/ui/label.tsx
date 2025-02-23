3ab44fb81c31e4c15d6d5df58d638d1e11865cd7
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-23 01:12:52 +0000

    ...

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,4 +1,5 @@
 // src/components/ui/label.tsx
+// src/components/ui/label.tsx
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cn } from "@/lib/utils"
