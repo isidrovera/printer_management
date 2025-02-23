ecbf2e5a62252fbf4fc02debf45b90fd87f9307f
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-23 01:18:14 +0000

    ...

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,7 +1,8 @@
 // src/lib/utils.ts
+// src/lib/utils.ts
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
